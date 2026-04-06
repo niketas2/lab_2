0d74a6c6c2fa84f4009b5ef76e20063eefd4721f
unknown <novikov.nicita123@gmail.com>
2026-04-06 15:34:42 +0300

    Change header name

--- lab_2_react/src/Components/Header.tsx
+++ lab_2_react/src/Components/Header.tsx
@@ -2,7 +2,7 @@
 export const Header = () => {
     return (
         <header className="rounded-2xl bg-blue-700 p-6 text-white shadow-md">
-            <h1 className="text-3xl font-bold leading-tight">Frontend Developer CV</h1>
+            <h1 className="text-3xl font-bold leading-tight">Frontend Developer CV!</h1>
             <p className="mt-4 text-xl font-medium">Новиков Микита</p>
             <p className="mt-1 text-sm text-blue-100">Україна</p>
         </header>
